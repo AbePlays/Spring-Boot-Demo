869e6efb31025738824c10dfcb34a6e857549b0a
Abe <abhi.rawat456@gmail.com>
2021-01-16 21:18:21 +0530

    Implemented all methods

--- src/main/java/com/abe/springboot/crud/rest/EmployeeRestController.java
+++ src/main/java/com/abe/springboot/crud/rest/EmployeeRestController.java
@@ -3,8 +3,12 @@ package com.abe.springboot.crud.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,4 +41,48 @@ public class EmployeeRestController {
 		
 		return employee;
 	}
+	
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		
+		employee.setId(0);
+		
+		employeeService.save(employee);
+		
+		return employee;
+	}
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee employee) {
+		
+		employeeService.save(employee);
+		
+		return employee;
+		
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	public String deleteEmployee(@PathVariable int id) {
+		
+		Employee employee = employeeService.findById(id);
+		
+		if(employee == null) {
+			throw new RuntimeException("Employee id not found");
+		}
+		
+		employeeService.deleteById(id);
+		
+		return "Deletion successful";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
