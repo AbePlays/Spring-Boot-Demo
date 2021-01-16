5011562a6d3c17eacc7959e0b932923b971a3ef8
Abe <abhi.rawat456@gmail.com>
2021-01-16 20:13:24 +0530

    Bug Fixes

--- src/main/java/com/abe/springboot/crud/rest/EmployeeRestController.java
+++ src/main/java/com/abe/springboot/crud/rest/EmployeeRestController.java
@@ -4,25 +4,37 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.abe.springboot.crud.dao.EmployeeDAO;
 import com.abe.springboot.crud.entity.Employee;
+import com.abe.springboot.crud.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-	private EmployeeDAO employeeDAO;
+	private EmployeeService employeeService;
 	
 	@Autowired
-	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-		employeeDAO = theEmployeeDAO;
+	public EmployeeRestController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
 	
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
-		return employeeDAO.findAll();
+		return employeeService.findAll();
+	}
+	
+	@GetMapping("/employee/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		Employee employee = employeeService.findById(employeeId);
+		
+		if(employee == null) {
+			throw new RuntimeException("Employee id not found");
+		}
+		
+		return employee;
 	}
 }
--- src/main/java/com/abe/springboot/crud/service/EmployeeServiceImpl.java
+++ src/main/java/com/abe/springboot/crud/service/EmployeeServiceImpl.java
@@ -2,36 +2,45 @@ package com.abe.springboot.crud.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.abe.springboot.crud.dao.EmployeeDAO;
 import com.abe.springboot.crud.entity.Employee;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 	
-	private EmployeeService theEmployeeService;
+	private EmployeeDAO employeeDao;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDAO theEmployeeDao) {
+		employeeDao = theEmployeeDao;
+	}
 
 	@Override
 	@Transactional
 	public List<Employee> findAll() {
-		return theEmployeeService.findAll();
+		return employeeDao.findAll();
 	}
 
 	@Override
 	@Transactional
 	public Employee findById(int id) {
-		return theEmployeeService.findById(id);
+		return employeeDao.findById(id);
 	}
 
 	@Override
 	@Transactional
 	public void save(Employee employee) {
-		theEmployeeService.save(employee);
+		employeeDao.save(employee);
 	}
 
 	@Override
 	@Transactional
 	public void deleteById(int id) {
-		theEmployeeService.deleteById(id);
+		employeeDao.deleteById(id);
 	}
 
 }
